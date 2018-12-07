467406793e3bc1dfe76094fc256839605104e901
Dario van den Boom <dario.van-den-boom@stud.uni-due.de>
2018-12-07 17:06:47 +0100

    Added: Cameraflipper

---
+++ Hackathon/Assets/Scripts/CameraFlipper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraFlipper : MonoBehaviour {
+
+    //Die Frequenz (Wartezeit in Sekunden), in der die Kamera gekippt werden soll.
+    public float Frequency = 10f;
+    private float timeStamp;
+    private int oldRotation;
+
+	// Use this for initialization
+	void Start () {
+        timeStamp = Time.time;
+        oldRotation = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time >= timeStamp + Frequency)
+        {
+            int randomRotation = Random.Range(0, 3);
+
+            while (randomRotation == oldRotation)
+            {
+                randomRotation = Random.Range(0, 3);
+            }
+
+            transform.rotation = Quaternion.Euler(0, 0, randomRotation * 90f);
+
+            timeStamp = Time.time;
+        }
+	}
+}
